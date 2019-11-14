c49965d9a1c9e24471b7da7c08748d65c0842cfe
Crakenar <teothou@hotmail.fr>
2019-11-14 16:42:46 +0100

    yes

--- view/informations.view.php
+++ view/informations.view.php
@@ -8,20 +8,20 @@
 </header>
 
 
-    <div class="certificat medical">
-      <img src="../view/Images/certificatMedical.jpg" alt="image d'un certificat medical">
+    <div class="certificat_medical">
+      <img src="../view/Images/certificatMedical.jpg" alt="Image d'un certificat medical concernant la pratique de la boxe muay thaï">
       <p>Certificats Medical</p>
     </div>
     <p>-------------------------------------------------------</p>
-    <div class="Feuille d'inscription">
+    <div class="Feuille_d'inscription">
       <img src="../view/Images/FeuilleInscription.jpg" alt="Image montrant une feuille d'inscription remplie">
       <p>Feuille d'inscription REMPLIE</p>
       <button type="button" name="button"><a href="FicheInscription.html" download = "../view/Images/FeuilleInscription.jpg">Fiche d'Inscription</a></button>
     </div>
     <p>-------------------------------------------------------</p>
     <div class="Cheque">
-      <img src="../view/Images/cheque-specimen.png" alt="image d'un cheque remplis au nom de l'association">
-      <p>Cheque de 235 euros au nom de l'association</p>
+      <img src="../view/Images/cheque-specimen.png" alt="image d'un cheque de 235 euros au nom de l'association">
+      <p>Cheque de 235 euros au nom de l'association : Grenoble Muay Thaï</p>
     </div>
     
 <p>-------------------------------------------------------</p>
