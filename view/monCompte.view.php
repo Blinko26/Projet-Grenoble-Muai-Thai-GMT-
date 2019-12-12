41da991097dfff282b0d1bbebd50088855de673f
DUTREVE Calvin <dutrevec@hostname.iut2.upmf-grenoble.fr>
2019-12-12 17:50:00 +0100

    test

--- view/monCompte.view.php
+++ view/monCompte.view.php
@@ -1,19 +1,23 @@
 <?php include '../view/header.view.php' ?>
-<link rel="stylesheet" href="../framework/monCompte.css">
+  <link rel="stylesheet" href="../framework/monCompte.css">
 
-<img src="../view/Images/backgroundMonCompte.jpg" alt="Background" class="imgBackground">
-
-
-
-</header>
+  <img src="../view/Images/backgroundMonCompte.jpg" alt="Background" class="imgBackground">
 
 
+
+  </header>
+    <p>Veuillez entrer le mot de passe pour obtenir les codes d'accès au serveur central de la NASA :</p>
+    <form action="secret.php" method="post">
+        <p>
+        <input type="password" name="mot_de_passe" />
+        <input type="submit" value="Valider" />
+        </p>
+    </form>
     <ul>
       <li><p>Nom d'Utilisateur :</p> </li>
       <li><p>Mot De Passe</p> </li>
       <div class="boutons">
-        <button type="button" name="buttonConnecter">Se Connecter  </button>
-        <button type="button" name="buttonInscription a="../view/subscribe.php"">Pas Encore Inscrit</button>
+        <button type="button" name="buttonInscription ">Pas Encore Inscrit</button>
       </div>
     </ul>
 
