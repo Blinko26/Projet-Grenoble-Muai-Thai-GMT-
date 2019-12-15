cb32f3b1c43f346c075291152e3f83d196c634d5
DUTREVE Calvin <dutrevec@hostname.iut2.upmf-grenoble.fr>
2019-12-15 16:43:36 +0100

    dernier commit

--- model/Adherent.class.php
+++ model/Adherent.class.php
@@ -9,7 +9,6 @@ class Adherent {
   public $taille;
   public $paiement;
   public $certifMedical;
-<<<<<<< HEAD
   public $telephone;
 
   function getNumAdherent() : int{
@@ -44,41 +43,4 @@ class Adherent {
     return $this->certifMedical;
   }
 }
-=======
-
-  function getNumAdherent() : int{
-    return $this->numAdherent;
-  }
-  
-  function getNom() : string{
-    return $this->nom;
-  }
-  
-  function getPrenom() : string{
-    return $this->prenom;
-  }
-  
-  function getDateNaissance() : string{
-    return $this->dateNaissance;
-  }
-  
-  function getPoids() : int{
-    return $this->poids;
-  }
-  
-  function getTaille() : int{
-    return $this->taille;
-  }
-  
-  function getPaiement() : string{
-    return $this->paiement;
-  }
-  
-  function getCertifMedical() : string{
-    return $this->certifMedical;
-  }
-}
-
-
->>>>>>> 376d3a78cd6a64e5fa32fc03c553bbadeed5bbad
 ?>
