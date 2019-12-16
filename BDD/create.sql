bcafae7d48839703b2957e033e5392d95e1303b9
BRETON Emeric <bretonem@hostname.iut2.upmf-grenoble.fr>
2019-12-16 17:22:07 +0100

    BDD?

--- BDD/create.sql
+++ BDD/create.sql
@@ -1,24 +1,41 @@
-CREATE TABLE Utilisateur (
+CREATE TABLE User (
   numUtilisateur INT PRIMARY KEY NOT NULL,
   login VARCHAR(25),
   password VARCHAR(50),
   mail VARCHAR(50),
-  role VARCHAR(50)
+  role TEXT CHECK( role IN ('admin','moderateur','adherent', 'inscrit', 'responsableLegal') )
 );
 
-CREATE TABLE infoPerso (
+CREATE TABLE informationsResponsableLegal (
   numUtilisateur INT PRIMARY KEY NOT NULL,
   nom VARCHAR(50),
   prenom VARCHAR(50),
-  sexe CHAR(1),
+  telephone VARCHAR(10),
+  numEnfant INT NOT NULL,
+  FOREIGN KEY(numUtilisateur) REFERENCES User(numUtilisateur),
+  FOREIGN KEY(numEnfant) REFERENCES informationsPersonnellesMineur(numAdh)
+);
+
+CREATE TABLE informationsPersonnelles (
+  numAdh INT PRIMARY KEY NOT NULL,
+  nom VARCHAR(50),
+  prenom VARCHAR(50),
+  sexe TEXT CHECK( sexe IN ('F','H') ),
   dateNaissance DATE,
   poids INT NOT NULL,
   taille INT NOT NULL,
   paiement BOOLEAN,
   certifMedical BOOLEAN,
-  autorisationP BOOLEAN DEFAULT true,
   telephone VARCHAR(10),
-  FOREIGN KEY(numUtilisateur) REFERENCES Utilisateur(numUtilisateur)
+  FOREIGN KEY(numAdh) REFERENCES User(numUtilisateur)
+);
+
+CREATE TABLE informationsPersonnellesMineur (
+  numAdh INT PRIMARY KEY NOT NULL,
+  autorisationP BOOLEAN,
+  numParent INT NOT NULL,
+  FOREIGN KEY(numAdh) REFERENCES informationsPersonnelles(numAdh),
+  FOREIGN KEY(numParent) REFERENCES informationsResponsableLegal(numUtilisateur)
 );
 
 CREATE TABLE Commentaire (
@@ -29,7 +46,7 @@ CREATE TABLE Commentaire (
   nomUtilisateur VARCHAR(50) DEFAULT 'Utilisateur anonyme',
   dateCom DATE,
   contenuCom TEXT,
-  FOREIGN KEY(numUtilisateur, nomUtilisateur) REFERENCES Utilisateur(numUtilisateur, login),
+  FOREIGN KEY(numUtilisateur, nomUtilisateur) REFERENCES User(numUtilisateur, login),
   FOREIGN KEY(numArticle) REFERENCES Article(id),
   FOREIGN KEY(numComSuivant) REFERENCES Commentaire(numCom)
 );
@@ -42,9 +59,9 @@ CREATE TABLE Article (
   contenu TEXT
 );
 
-CREATE TABLE Objet (
+CREATE TABLE Media (
   id INT NOT NULL,
-  nomObjet VARCHAR(255),
-  type VARCHAR(6),
+  nomMedia VARCHAR(255),
+  type TEXT CHECK( type IN ('video', 'image') ),
   FOREIGN KEY(id) REFERENCES Article(id)
 );
