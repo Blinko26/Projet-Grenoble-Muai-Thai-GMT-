0653ed802db62724adaf62392ec5d55767a57960
BRETON Emeric <bretonem@hostname.iut2.upmf-grenoble.fr>
2020-01-02 17:11:33 +0100

    Fin commentaires BDD, à vérifier si besoin.

--- BDD/import.sql
+++ BDD/import.sql
@@ -4,3 +4,5 @@
 .import informationsPersonnelles.txt informationsPersonnelles
 .import user.txt User
 .import informationsResponsableLegal.txt informationsResponsableLegal
+
+-- Fichier servant à recréer plus facilement chacune des tables de la base de données, dans le langage sqlite3.
