072446b85f050c37e0648346f928730c73de816e
Blinko26 <bruno.viellard@gmail.com>
2020-01-08 13:35:34 +0100

    Inscription Boutons CSS

--- view/inscription.view.php
+++ view/inscription.view.php
@@ -1,6 +1,6 @@
 <?php session_start();
 include '../controler/header.ctrl.php'; ?>
-<link rel="stylesheet" href="../framework/informations.css">
+<link rel="stylesheet" href="../framework/inscription.css">
 <img src="../view/Images/backgroundInformation.jpg" alt="Background" class="imgBackground">
 </header>
 
@@ -13,16 +13,15 @@ include '../controler/header.ctrl.php'; ?>
 
 
 <h2>Le Certificat medical</h2>
-<div class="certificat_medical">
-<img src="../view/Images/certificatMedical.jpg" alt="Image d'un certificat medical concernant la pratique de la boxe muay thaï">
-<button type="button" name="button"><a href="FicheInscription.html" download ="../view/Images/certificatMedical.jpg">Certificat Medical Type</a></button>
-
+<div class="certificatmedical">
+<img src="../view/Images/certificatMedical.jpg" alt="Certificat Medical">
+<button class="button" type="button" name="button"><a href="FicheInscription.html" download ="../view/Images/certificatMedical.jpg">Certificat Medical Type</a></button>
 </div>
 
-<h2>Feuille Inscription</h2>
-<div class="Feuille_d'inscription">
-<img src="../view/Images/FeuilleInscription.jpg" alt="Feuille Inscription" height = "600" width = "600">
-<button type="button" name="button"><a href="FicheInscription.html" download = "../view/Images/FeuilleInscription.jpg">Fiche d'Inscription</a></button>
+<h2>Feuille D'inscription</h2>
+<div class="feuilleInscription">
+<img src="../view/Images/FeuilleInscription.jpg" alt="Feuille Inscription">
+<button class="button" type="button" name="button"><a href="FeuilleInscription.html" download ="../view/Images/FeuilleInscription.jpg">Feuille Inscription</a></button>
 </div>
 
 <h2>Cheque</h2>
